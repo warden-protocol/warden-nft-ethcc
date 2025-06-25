4a053cc9eb3703f49904de9b8ee4847a24d61801
Pavel <pavsmel@hotmail.com>
2025-06-25 16:26:07 +0300

    implement deployment script

--- README.md
+++ README.md
@@ -22,6 +22,11 @@ $ forge fmt
 
 ### Deploy
 
-```shell
-$ forge script script/Counter.s.sol:CounterScript --rpc-url <your_rpc_url> --private-key <your_private_key>
-```
+1. Set `DEPLOYER_PRIVATE_KEY` env variable
+2. Run
+    ```bash
+    $ forge script script/WardenNFTFactory.s.sol:DeployFactory \
+    --sig "run(address, uint32, bytes32)" <mailbox_contract_address> <domain> <destination_address> \
+    --rpc-url <your_rpc_url> \
+    --broadcast
+    ```
---
+++ script/WardenNFTFactory.s.sol
@@ -0,0 +1,35 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import {Script, console} from "forge-std/Script.sol";
+import {VmSafe} from "forge-std/Vm.sol";
+import {WardenNFTFactory} from "../src/WardenNFTFactory.sol";
+
+contract DeployFactory is Script {
+    string public constant DEPLOYMENT_FILE = "factory.json";
+    WardenNFTFactory public wardenNFTFactory;
+
+    function run(address mailbox, uint32 domain, bytes32 target) public {
+        console.log("mailbox: %s", mailbox);
+        console.log("domain: %s", domain);
+        console.logBytes32(target);
+        uint256 privateKey = vm.envUint("DEPLOYER_PRIVATE_KEY");
+        vm.startBroadcast(privateKey);
+        wardenNFTFactory = new WardenNFTFactory(mailbox, domain, target);
+        vm.stopBroadcast();
+        _saveDeployment();
+    }
+
+    function _saveDeployment() private {
+        string memory path = _getDeploymentPath(DEPLOYMENT_FILE);
+        string memory output = vm.serializeAddress("", "WardenNFTFactory", address(wardenNFTFactory));
+        vm.writeJson(output, path);
+    }
+
+    function _getDeploymentPath(string memory deploymentName) internal view returns (string memory) {
+        string memory root = vm.projectRoot();
+        string memory chainDir = string.concat(vm.toString(block.chainid));
+        string memory path = string.concat(root, "/script/deployment/", chainDir, "/", deploymentName);
+        return path;
+    }
+}
