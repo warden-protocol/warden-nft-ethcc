6827de136338aa10ac9f1ba21c3ff9bf59f80cb5
Pavel <pavsmel@hotmail.com>
2025-06-25 16:58:04 +0300

    fix compiler versions

--- script/WardenNFTFactory.s.sol
+++ script/WardenNFTFactory.s.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.13;
+pragma solidity 0.8.30;
 
 import {Script, console} from "forge-std/Script.sol";
 import {VmSafe} from "forge-std/Vm.sol";
